1e5f1c6ed3e1929baecc9312d71e17132d860306
Shahriyar <mdshahriyaralam552@gmail.com>
2021-08-07 13:42:35 +0600

    Fixed blueprint importing

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="quart-admin",
-    version="2.0.1",
+    version="2.0.3",
     description="A tool to create quart projects faster with a scalable setup",  # Optional
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,17 +19,14 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3 :: Only",
     ],
     entry_points={"console_scripts": ["quart-admin = quart_admin:cli"]},
     keywords="quart, quart-admin",
     packages=find_packages(),
-    python_requires=">=3.6, <4",
+    python_requires=">=3.7, <4",
     install_requires=["quart", "click", "python-slugify"],
     project_urls={
         "Bug Reports": "https://github.com/shahriyardx/quart-admin/issues",
